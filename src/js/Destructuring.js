23f2977e5329b78b856afb2f7eb976bff2a00e89
Roman Prilepsky <r616on@gmail.com>
2021-05-03 22:07:57 +0300

    task_1.2

--- src/js/Destructuring.js
+++ src/js/Destructuring.js
@@ -1,13 +1,7 @@
 export default function Destructuring(obj) {
   const { special: specialArr } = obj;
   for (let i = 0; i < specialArr.length; i += 1) {
-    let k = 0;
-    for (const prop in specialArr[i]) {
-      if (prop === 'description') {
-        k += 1;
-      }
-    }
-    if (k === 0) {
+    if (!('description' in specialArr[i])) {
       specialArr[i].description = 'Описание недоступно';
     }
   }
